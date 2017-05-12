f0fbdd0fe9fb95b99e5d365e85c3fd4e6f6fd204
Victor Wibisono <victor@eatigo.com>
2017-05-12 18:16:25 +0800

    Added service worker.

--- scripts/app.js
+++ scripts/app.js
@@ -206,4 +206,13 @@
       app.getForecast(key);
     });
   };
+
+  if ('serviceWorker' in navigator) {
+    navigator.serviceWorker
+     .register('/service-worker.js')
+     .then(function() { 
+        console.log('Service Worker Registered'); 
+      });
+  }
+
 })();
